cbfc40f49d85ee3ae672ca4c4ebc8a14a098167f
Md Hasibur Rahman <hasiburrahman875@gmail.com>
2023-09-04 17:53:00 -0500

    Update PageRank.java

--- src/PageRank.java
+++ src/PageRank.java
@@ -33,14 +33,31 @@ public class PageRank {
 		}
 	}
 
+	/* From the mapper we will get,
+ 		1st iter: (a,1) (b,1/4) (c,1/4) (d,1/4) (e, 1/4)
+   		2nd iter: (b,1) (c,1/3) (d, 1/3) (e, 1/3)
+     		3rd iter: (c,1) (d,1/2) (e, 1/2)
+       		4th iter: (d,1) (c,1/2) (b,1/2)
+	 	5th iter: (e,1) (c,1/2) (b,1/2)
+ 	
+  	After shuffling we will get the actual output which will be fed into the reducer function,
+   		<a,(1)>
+     		<b,(1/4,1,1/2,1/2)>
+       		<c,(1/4,1/3,1,1/2,1/2)>
+  		<d,(1/4,1/3,1/2,1)>
+ 		<e,(1/4,1/3,1/2,1)>
+ 	*/
+	
+
 	public static class MyReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
 
 		@Override
 		public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
 			
 			double pagerank = 0.0;
-			for (DoubleWritable value : values) {
-				pagerank += Double.parseDouble(value.toString());
+			//for b, 1/4+1+1/2+1/2 = 2.25
+			for (DoubleWritable value : values) { 
+				pagerank += Double.parseDouble(value.toString()); 
 			}
 			context.write(key, new DoubleWritable(pagerank));
 		}
